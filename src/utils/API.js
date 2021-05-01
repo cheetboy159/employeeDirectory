f6e850a6ed747ed1c07221b947655eae9871156e
Mena Wahba <menawahba@gmail.com>
2021-05-01 17:35:06 -0400

    adding files for header and search bar

---
+++ src/utils/API.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const URL = "https://randomuser.me/api/?results=20&nat=us"
+export default {
+    getEmployees: function () {
+        return axios.get(URL);       
+    }
+}
