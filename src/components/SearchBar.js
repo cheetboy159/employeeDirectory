d2ac09c8bfe62d5cacac9133cec55fc02a657902
Mena Wahba <menawahba@gmail.com>
2021-05-01 19:39:21 -0400

    uploading employee container and table

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,20 +2,12 @@ import React from "react";
 
 function SearchBar() {
     return (
-        <div className="input-group mb-3">
-            <input
-                type="text"
-                className="form-control"
-                placeholder="Search by Name"
-                aria-label="earch by Name"
-                aria-describedby="button-addon2" />
-            <div className="input-group-append">
-                <button
-                    className="btn btn-outline-secondary"
-                    type="button"
-                    id="button-addon2">Button</button>
+        <>
+            <div className="input-group mb-3">
+                <input type="text" className="form-control" placeholder="Search by Name" aria-label="earch by Name" aria-describedby="button-addon2"/>
+                <div className="input-group-append"> <button className="btn btn-outline-secondary" type="button" id="button-addon2"> Search </button> </div>
             </div>
-        </div>
+        </>
     );
 }
 export default SearchBar;
